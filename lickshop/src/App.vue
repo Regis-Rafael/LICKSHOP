196c0aeb5135013b4e3c11a7912ffc61438cf3f3
Regis-Rafael <rafaextreme14@gmail.com>
2024-12-06 18:40:19 -0300

    hihi

--- lickshop/src/App.vue
+++ lickshop/src/App.vue
@@ -1,26 +1,62 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app">
+    <!-- Renderiza HeaderLogged se o usuário estiver logado, caso contrário renderiza HeaderUnlogged -->
+    <HeaderLogged v-if="isLoggedIn" />
+    <HeaderUnlogged v-else />
+    
+    <main>
+      <router-view />
+    </main>
+
+    <FooterDown />
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderUnlogged from './components/HeaderUnlogged.vue';
+import FooterDown from './components/FooterDown.vue';
+import HeaderLogged from './components/HeaderLogged.vue';
+import localStorageService from './services/localStorage'; // Serviço de autenticação
 
 export default {
-  name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    HeaderLogged,
+    HeaderUnlogged,
+    FooterDown,
+  },
+
+  data() {
+    return {
+      isLoggedIn: localStorageService.isAuthenticated(), // Verifica se o usuário está logado
+    };
+  },
+
+  created() {
+    // Atualiza o estado de login se necessário (ex: quando o usuário loga ou desloga)
+    this.isLoggedIn = localStorageService.isAuthenticated();
+  },
+
+  methods: {
+    updateLoginStatus() {
+      // Método para atualizar o estado de login
+      this.isLoggedIn = localStorageService.isAuthenticated();
+    }
+  },
+};
 </script>
 
-<style>
+<style scoped>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh; 
+}
+
+main {
+  flex: 1; 
+}
+
+footer {
+  flex-shrink: 0;
 }
 </style>
--- lickshop/vue.config.js
+++ lickshop/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    port: 3000, // Define a porta do servidor de desenvolvimento
+    proxy: {
+      '/api': {
+        target: 'https://api.lickshop.acilab.com.br', // O endereço da sua API
+        changeOrigin: true, // Muda a origem da requisição para evitar CORS
+        pathRewrite: {
+          '^/api': '', // Remove '/api' do início da URL da requisição
+        },
+      },
+    },
+  },
 })
